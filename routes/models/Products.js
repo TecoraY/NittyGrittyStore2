1c2d730d68110504277fa89ac4beb3f506deedbe
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-06-01 02:44:32 -0400

    back on track

---
+++ routes/models/Products.js
@@ -0,0 +1,20 @@
+//database of products to export
+
+const pokemon = [
+    {name: String,
+    description: String,
+    img: String,
+    price: Number,
+    quantity: Number,}
+ ];
+ //need to export database
+ module.exports= products;
+
+ /*{name: "Knit Set", img: "https://i.pinimg.com/originals/97/54/aa/9754aa54891927dba0e7d436d4aa7052.jpg"},
+    {name: "Knit Crop Top", img: "https://i.pinimg.com/736x/21/1b/d1/211bd10c92b436686df2582c63507dc8.jpg"},
+    {name: "Baby Fox", img: "https://dabblesandbabbles.com/wp-content/uploads/2019/06/Pocket-Fox.jpg"},
+    {name: "Knit Vehicle", img: "https://crochet-news.com/wp-content/uploads/2021/05/happy-little-car-truck-and-plane-crochet-pattern.jpg"},
+    {name: "Knit Turtle", img: "https://crochet-news.com/wp-content/uploads/2021/05/crochet-turtle-toy-pattern.jpg"},
+    {name: "HoneyComb Blanket", img: "https://www.stylecraft-yarns.co.uk/Content/Images/Patterns/Bellissima%20DK/9614-1_zoom.jpg"},
+    {name: "Knit Wall Art", img: "https://5.imimg.com/data5/BB/WG/SW/SELLER-13179332/draped-knee-length-skirt-500x500.jpg"},
+    {name: "Round Knit Pillow", img: "https://www.whistleandivy.com/wp-content/uploads/2019/01/superchunkycrochetpillowfreecrochetpattern19-600x900.jpg"}*/
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+const mongoose =require('mongoose');
 const express=require('express');
-//connect database by telling where the database is defined/stored
-const connectDB=require("./config/database");
+const dotenv=require('dotenv');
+dotenv.config({
+    path:"./config.env",
+});
+const PORT= 3000;
+
 const app =express();
-//call the database, do here for app to run properly
-connectDB();
+const knittyDB=mongoose.connect(()=>{
+    console.log("DATABASE IS UP AND RUNNING...");
+});
 app.use(express.json({extended:false}));
 app.get('/', (request, response)=> response.send('store up and running'));
 
@@ -12,6 +18,6 @@ app.get('/', (request, response)=> response.send('store up and running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/products', require('./routes/api/products'));
 
-const PORT= process.env.PORT|| 5000;
+
 
 app.listen(PORT, ()=> console.log(`server started on ${PORT}`));
